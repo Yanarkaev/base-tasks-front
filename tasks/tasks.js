41458e307c30fd08ec963499e139e70b67d3cd2f
khasss <110416456+Yanarkaev@users.noreply.github.com>
2025-08-09 11:22:46 +0300

    Add files via upload

---
+++ tasks/tasks.js
@@ -0,0 +1,80 @@
+
+// 1. Таблица умножения на 5
+function printTable(n) {
+    // ваш код
+}
+
+
+
+// 2. Сумма чисел от 1 до N
+function sumToN(n) {
+    // ваш код
+}
+
+
+
+// 3. Четные числа от 1 до 20
+function printEvenNumbers() {
+    // ваш код
+}
+
+
+
+// 5. Найти минимальное число в массиве
+function findMin(arr) {
+    // ваш код
+}
+
+
+
+function reverseArray(arr) {
+    // ваш код
+}
+
+
+
+// 10. Факториал
+function factorial(n) {
+    // ваш код
+}
+
+
+// 11. Угадай число (от 1 до 10)
+function guessNumber(userGuess) {
+    // ваш код
+}
+
+
+
+// 12. Поиск элемента в массиве
+function findFruit(fruit) {
+    // ваш код
+}
+
+
+
+// 13. Сумма элементов массива
+function sumArray(arr) {
+    // ваш код
+}
+
+
+
+// 14. Счетчик пробелов
+function countSpaces(str) {
+    // ваш код
+}
+
+
+
+// 15. Обратный отсчет с шагом
+function countdown(start, step) {
+    // ваш код
+}
+
+
+
+// 20. Среднее значение массива
+function average(arr) {
+    // ваш код
+}
